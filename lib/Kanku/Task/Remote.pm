298d7e2ceb58ba632aecacef00411556babe0f7b
M0ses <schreiner@suse.de>
2017-02-26 16:04:47 +0000

    [core] 'use' cleanup and license for K::Task::Remote

--- lib/Kanku/Task/Remote.pm
+++ lib/Kanku/Task/Remote.pm
@@ -1,5 +1,20 @@
+# Copyright (c) 2016 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program (see the file COPYING); if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+#
 package Kanku::Task::Remote;
-
 
 =head1 NAME
 
@@ -23,10 +38,9 @@ use Moose;
 
 our $VERSION = "0.0.1";
 
-use FindBin;
-use lib "$FindBin::Bin/lib";
+with 'Kanku::Roles::Logger';
+
 use Data::Dumper;
-use POSIX;
 use JSON::XS;
 use Kanku::MQ;
 use Try::Tiny;
