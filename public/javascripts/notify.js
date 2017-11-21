17d2e0d6b753dd55005a08bffe87888f0d242a25
Frank Schreiner <schreiner@suse.de>
2017-11-21 16:14:21 +0100

    [web] colorized icons in notifications

--- public/javascripts/notify.js
+++ public/javascripts/notify.js
@@ -1,13 +1,27 @@
 var mySocket = new WebSocket(ws_url);
 var token = Cookies.get("kanku_notify_session");
 
+// Preloading images
+$.each(['32', '64'], function(index, size) {
+  $.each(['', '-danger'], function(index, ext) {
+    image = new Image();
+    image.src = uri_base + '/images/' + size + '/kanku' + ext + '.png';
+  });
+});
+
 mySocket.onmessage = function (evt) {
   console.log( "Got message " + evt.data );
   data = JSON.parse(evt.data);
+  var ico_ext = '';
+  if ( data.result == 'failed' ) {
+    ico_ext = '-danger'
+  }
+  var ico = uri_base + '/images/64/kanku' + ico_ext + '.png';
+  console.log(ico);
   Notification.requestPermission(function() {
     var n = new Notification(data.title, {
 	body: data.body,
-	icon: './favicon.ico' // optional
+	icon: ico
     });
     n.onclick = function() {
         window.open(data.link, 'newwindow', "menubar=no");
@@ -19,29 +33,35 @@ mySocket.onmessage = function (evt) {
 
 mySocket.onopen = function(evt) {
   console.log("opening Socket");
-  setTimeout(
-    function() {
-      var msg = '{"token":"'+ token +'"}';
-      console.log("sending token " + msg);
-      console.log(msg)
-      mySocket.send(msg);
-    },
-    2000
-  );
-  setTimeout(
-    function() {
-      mySocket.send('{"bounce":"Opened WebSocket successfully!"}');
-    },
-    2000
-  );
+  var ico = uri_base + '/images/32/kanku.png';
+  Notification.requestPermission(function() {
+    $("#favicon").attr("href",ico);
+    setTimeout(
+      function() {
+	var msg = '{"token":"'+ token +'"}';
+	console.log("sending token " + msg);
+	console.log(msg)
+	mySocket.send(msg);
+      },
+      2000
+    );
+    setTimeout(
+      function() {
+	mySocket.send('{"bounce":"Opened WebSocket successfully!"}');
+      },
+      2000
+    );
+  });
 };
 
 mySocket.onclose = function(evt) {
   Notification.requestPermission(function() {
     var m = 'Closed WebSocket - no more messages will be displayed';
+    var ico = uri_base + '/images/64/kanku-danger.png';
+    $("#favicon").attr("href",ico);
     var n = new Notification('Kanku Desktop Notification', {
 	body: m,
-	icon: './favicon.ico' // optional
+	icon: ico
     });
     $("#content").text(m);
     n.onclick = function() {
@@ -50,6 +70,8 @@ mySocket.onclose = function(evt) {
     };
     setTimeout(n.close.bind(n), 20000);
   });
+  var ico = uri_base + '/images/32/kanku-danger.png';
+  $("#favicon").attr("href",ico);
 };
 
 if (! window.Notification ) {
