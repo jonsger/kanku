dde4807cdaa7ec6a44f7ded4a94820d7e824e69d
Frank Schreiner <schreiner@suse.de>
2018-07-12 13:42:10 +0000

    [web] fix rendering problem in admin interface

--- lib/Kanku.pm
+++ lib/Kanku.pm
@@ -71,13 +71,9 @@ get '/login/denied' => sub {
 };
 
 get '/admin' => requires_role Admin =>  sub {
-  my @users = schema('default')->resultset('User')->search();
-  my @roles = schema('default')->resultset('Role')->search();
   template 'admin', {
       %{get_defaults_for_views()},
       kanku => {module => 'Administration'},
-      users => \@users,
-      roles => \@roles,
   };
 };
 
