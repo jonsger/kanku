7190f58291fb96ecc711d9967c86475c17d83bd3
Frank Schreiner <schreiner@suse.de>
2018-08-09 23:43:16 +0200

    [web] removed obsolete ajaxsetup.js

--- public/javascripts/ajaxsetup.js
+++
@@ -1,6 +0,0 @@
-$.ajaxSetup({
-   'error' : function (xhr, state, error) {
-      $('#content').append("<h2>An error occured while ajax call</h2>");
-      $('#content').append("<pre>"+xhr.responseJSON.exception+"</pre>");
-   }
-});
